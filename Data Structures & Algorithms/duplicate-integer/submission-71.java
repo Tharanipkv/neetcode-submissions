3155e114e34bacc7b4903db89ce7785a67d001ec
Tharanipkv <62536252+Tharanipkv@users.noreply.github.com>
2026-06-29 20:09:37 +0530

    Add: duplicate-integer - submission-71

---
+++ Data Structures & Algorithms/duplicate-integer/submission-71.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        
+        // return Arrays.stream(nums).distinct().count() < nums.length;
+       
+       
+        Arrays.sort(nums);
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]==nums[i-1]){
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
